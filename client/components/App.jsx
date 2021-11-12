260eddfdaf27792ac8e1ee4b2614f4127406f75a
troy-may <troy.may@fortuneunmasked.com>
2021-11-12 15:06:29 +1300

    expanded info route added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import { gecko } from '../api'
 
 import Trending from './Trending'
 import CoinInfo from './CoinInfo'
+import ExpandInfo from './ExpandInfo'
 
 function App () {
   const [geckoState, setGecko] = useState('')
@@ -26,6 +27,7 @@ function App () {
       <h2>{geckoState}</h2>
       <Route path='/' exact component={Trending} />
       <Route path='/:id' component={CoinInfo} />
+      <Route path='/:id/info' component={ExpandInfo} />
     </>
   )
 }
