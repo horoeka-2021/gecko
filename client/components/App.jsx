3ea79710c7d650d78180ea06d9a04cd13cc1a952
bz888 <ziyu.ben158@gmail.com>
2021-11-12 15:13:10 +1300

    conflicts?

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,30 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
-import { gecko } from '../api'
+// import { gecko } from '../api'
 
 import Trending from './Trending'
 import CoinInfo from './CoinInfo'
+
 import ExpandInfo from './ExpandInfo'
 
-function App () {
-  const [geckoState, setGecko] = useState('')
+import ThreeJS from './ThreeJS'
 
-  // ping gecko api
-  useEffect(() => {
-    gecko()
-      .then(res => {
-        // console.log('res: ', res)
-        setGecko(res.gecko_says)
-        return null
-      })
-      .catch((err) => {
-        console.error(err.message)
-      })
-  }, [])
+function App () {
+  // // ping gecko api
+  // useEffect(() => {
+  //   gecko()
+  //     .then(res => {
+  //       // console.log('res: ', res)
+  //       setGecko(res.gecko_says)
+  //       return null
+  //     })
+  //     .catch((err) => {
+  //       console.error(err.message)
+  //     })
+  // }, [])
 
   return (
     <>
-      <h2>{geckoState}</h2>
+      {/* <Route path='/' component={ThreeJS}/> */}
       <Route path='/' exact component={Trending} />
       <Route path='/:id' component={CoinInfo} />
       <Route path='/:id/info' component={ExpandInfo} />
--- client/components/Trending.jsx
+++ client/components/Trending.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 import { getTrending } from '../api'
 export default function Trending () {
   const [data, setData] = useState({ coins: [] })
+  const [style, setStyle] = useState({
+    display: 'block'
+  })
   //   console.log(data)
 
   function handleClicked () {
@@ -10,6 +13,7 @@ export default function Trending () {
       .then(ele => {
         console.log(ele)
         setData(ele)
+        setStyle({ display: 'none' })
         return null
       })
       .catch(err => console.error(err))
@@ -17,14 +21,17 @@ export default function Trending () {
 
   return (
     <div className='bullsEye'>
-      {data.coins.map(ele => {
-        return (
-          <Link to={`/${ele.item.id}`} key={ele.item.coin_id}>
-            <img src={ele.item.large} alt={ele.item.name}></img>
-          </Link>
-        )
-      })}
-      <div className='trend-btn' onClick={handleClicked}></div>
+      <div className='ring2'>
+        <h2 className='title' style={style}>coinGun</h2>
+        {data.coins.map((ele, idx) => {
+          return (
+            <Link to={`/${ele.item.id}`} key={ele.item.coin_id} >
+              <img src={ele.item.large} alt={ele.item.name} ></img>
+            </Link>
+          )
+        })}
+        <div className='trend-btn' onClick={handleClicked} style={style}></div>
+      </div>
     </div>
   )
 }
